a047e55165ae55763650ade2ef06ff33fbc258fb
asta63 <stajkovic663a@htl-ottakring.ac.at>
2018-05-11 13:02:56 +0200

    Aktuellste Version 13:00 11.05.2018

--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -1,5 +1,3 @@
-//andreas
-
 $(function() {
     $('#calendar').fullCalendar({
         //Design [hoehe]
@@ -11,16 +9,11 @@ $(function() {
           center: 'title',
           right: 'month,agendaWeek,agendaDay'
         },
-        dayClick: function(date, jsEvent, calEvent) {
-          //alert(date.format("DD-MM-YYYY") + ' ausgewählt');
-          $('#exampleModal').modal('show');
-        },
         select: function(startDate, endDate) {
           //alert('Vom ' + startDate.format("DD-MM-YYYY") + ' bis zum ' + endDate.subtract(1, "days").format("DD-MM-YYYY") + ' ausgewählt.');
           $('#datumvon').val(startDate.format("DD-MM-YYYY"));
           $('#datumbis').val(endDate.subtract(1, "days").format("DD-MM-YYYY"));
           $('#exampleModal').modal('show');
-          console.log('funzt2');
         },
         //Funktion [vorhandes event info return mittels alert]
           defaultDate: $.datepicker.formatDate('yy/mm/dd', new Date()),
@@ -38,16 +31,6 @@ $(function() {
     });
 });
 
-/*$(function() {
-    $('#calendar').fullCalendar({
-        defaultDate: '2018-05-04',
-        defaultView: 'agendaWeek',
-        events: [
-          {
-            start: '2018-05-04T10:00:00',
-            end: '2018-05-08T16:00:00',
-            rendering: 'background'
-          }
-        ]
-      });
-});*/
+$('#btnAdd').click(function(){
+//add
+});
